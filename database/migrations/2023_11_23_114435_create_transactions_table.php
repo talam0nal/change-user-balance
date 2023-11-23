645dc4e69ff71c7851269879434b4fcdf4c7ffed
Николай Моргунов <talam0nal@gmail.com>
2023-11-23 18:21:25 +0300

    fix migration

--- database/migrations/2023_11_23_114435_create_transactions_table.php
+++ database/migrations/2023_11_23_114435_create_transactions_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->integer('user_id');
-            $table->integer('amount');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('user_id');
+            $table->integer('amount');
         });
     }
 
